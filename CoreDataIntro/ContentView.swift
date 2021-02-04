a11213cafc942d0bd6c2627ec408c3bd44f31bbb
David Svensson <davidsvson@gmail.com>
2021-02-04 13:39:26 +0100

    shopping list

--- CoreDataIntro/ContentView.swift
+++ CoreDataIntro/ContentView.swift
@@ -10,35 +10,63 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        
+        VStack {
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        if let name = item.name {
+                            Text("\(name)")
+                        }
+                        Spacer()
+                        Button(action: {
+                            toggle(item: item)
+                        }) {
+                            Image(systemName: item.done ? "checkmark.square" : "square")
+                        }
+                    }
+                }
+                .onDelete(perform: deleteItems)
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            HStack {
+                #if os(iOS)
+                EditButton()
+                #endif
+                Spacer()
+                Button(action: addItem) {
+                    Label("Add Item", systemImage: "plus")
+                }
             }
         }
     }
-
+    
+    private func toggle(item: Item) {
+        item.done.toggle()  // = !item.done
+        
+        do {
+            try viewContext.save()
+        } catch {
+            print("Error toggling object")
+        }
+        
+    }
+    
+    
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            newItem.name = "morot"
+            newItem.done = false
+            
             do {
                 try viewContext.save()
             } catch {
@@ -49,11 +77,17 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            //offsets.map { items[$0] }.forEach(viewContext.delete)
+            
+            for index in offsets {
+                let item = items[index]
+                viewContext.delete(item)
+            }
+            
+            
             do {
                 try viewContext.save()
             } catch {
